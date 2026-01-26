1e2344e7b28c6144598a5acbcdbf142997fb306e
Andrew Heinlein <andrew_heinlein@bradycorp.com>
2026-01-26 10:30:43 -0600

    more work

--- WebApiWithMcp/Controllers/WeatherForecastController.cs
+++ WebApiWithMcp/Controllers/WeatherForecastController.cs
@@ -15,7 +15,7 @@ namespace WebApiWithMcp.Controllers
 
 		private readonly IWeatherService _weatherService;
 
-		[HttpGet(Name = "GetWeatherForecast")]
+		[HttpGet]
 		public IEnumerable<WeatherForecast> Get([FromQuery] string zipCode)
 		{
 			return _weatherService.GetForecastAsync(zipCode);
