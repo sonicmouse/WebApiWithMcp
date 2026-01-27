1fd85a9ba0286bbc114b18e440b92c7fdf4d8048
Andrew Heinlein <andrew_heinlein@bradycorp.com>
2026-01-27 10:12:05 -0600

    cleanup

--- WebApiWithMcp/Program.cs
+++ WebApiWithMcp/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddTransient<IWeatherService, WeatherService>();
 
+// ----------------------------------------------------------
 var app = builder.Build();
 
 app.UseHttpsRedirection();
